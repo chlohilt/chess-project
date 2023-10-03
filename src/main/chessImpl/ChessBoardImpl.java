85f975ad5cf36fe405c649fafda19de3099ac05b
chlohilt <hilton.chloe.y@gmail.com>
2023-10-02 22:37:10 -0600

    Chess Board tests all passing

--- src/main/chessImpl/ChessBoardImpl.java
+++ src/main/chessImpl/ChessBoardImpl.java
@@ -16,18 +16,19 @@ public class ChessBoardImpl implements ChessBoard {
   public void resetBoard() {
     for (int i = 0; i < board.length; ++i) {
       for (int j = 0; j < board.length; ++j) {
-        if (i == 1) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN); } //PAWNS
-        else if (i == 6) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN); }
-        else if (i == 0 && (j == 0 || j == 7)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK); } // ROOKS
-        else if (i == 7 && (j == 0 || j == 7)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK); }
-        else if (i == 0 && (j == 1 || j == 6)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT); } // KNIGHTS
-        else if (i == 7 && (j == 1 || j == 6)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT); }
-        else if (i == 0 && (j == 2 || j == 5)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP); } // BISHOPS
-        else if (i == 7 && (j == 2 || j == 5)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP); }
-        else if (i == 0 && j == 3) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN); } // QUEENS
-        else if (i == 7 && j == 3) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN); }
-        else if (i == 0 && j == 4) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING); } // KINGS
-        else if (i == 7 && j == 4) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING); }
+        if (i == 1) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN); } //PAWNS
+        else if (i == 6) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN); }
+        else if (i == 0 && (j == 0 || j == 7)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK); } // ROOKS
+        else if (i == 7 && (j == 0 || j == 7)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK); }
+        else if (i == 0 && (j == 1 || j == 6)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT); } // KNIGHTS
+        else if (i == 7 && (j == 1 || j == 6)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT); }
+        else if (i == 0 && (j == 2 || j == 5)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP); } // BISHOPS
+        else if (i == 7 && (j == 2 || j == 5)) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP); }
+        else if (i == 0 && j == 3) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN); } // QUEENS
+        else if (i == 7 && j == 3) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN); }
+        else if (i == 0 && j == 4) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING); } // KINGS
+        else if (i == 7 && j == 4) { board[i][j] = new ChessPieceImpl(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING); }
+        else { board[i][j] = null; }
       }
     }
   }
