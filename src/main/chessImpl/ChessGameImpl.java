6d363299175ecff0b1def2f28588ee1397e6f5b6
chlohilt <hilton.chloe.y@gmail.com>
2023-10-04 23:35:40 -0600

    6 more tests to go!

--- src/main/chessImpl/ChessGameImpl.java
+++ src/main/chessImpl/ChessGameImpl.java
@@ -24,7 +24,36 @@ public class ChessGameImpl implements ChessGame {
     if (chessBoard.getPiece(startPosition) == null) {
       return null;
     } else {
-      return chessBoard.getPiece(startPosition).pieceMoves(chessBoard, startPosition);
+      Collection<ChessMove> movesBeforeInCheck = chessBoard.getPiece(startPosition).pieceMoves(chessBoard, startPosition);
+      Collection<ChessMove> validMoves = new HashSet<>();
+      TeamColor teamColor = chessBoard.getPiece(startPosition).getTeamColor();
+      ChessPiece.PieceType pieceType = chessBoard.getPiece(startPosition).getPieceType();
+      ChessPiece originalPiece = chessBoard.getPiece(startPosition);
+      TeamColor opposingColor=null;
+      if (teamColor == TeamColor.WHITE) {
+        opposingColor = TeamColor.BLACK;
+      } else if (teamColor == TeamColor.BLACK) {
+        opposingColor = TeamColor.WHITE;
+      }
+
+      chessBoard.removePiece(startPosition);
+      for (ChessMove checkMove: movesBeforeInCheck) {
+        ChessPieceImpl piece=new ChessPieceImpl(teamColor, pieceType);
+        ChessPiece enemyPiece = null;
+        if (chessBoard.getPiece(checkMove.getEndPosition()) != null && chessBoard.getPiece(checkMove.getEndPosition()).getTeamColor() == opposingColor) {
+          enemyPiece = chessBoard.getPiece(checkMove.getEndPosition());
+        }
+        chessBoard.addPiece(checkMove.getEndPosition(), piece);
+        if (!isInCheck(teamColor)) {
+          validMoves.add(checkMove);
+        }
+        chessBoard.removePiece(checkMove.getEndPosition());
+        if (enemyPiece != null) {
+          chessBoard.addPiece(checkMove.getEndPosition(), enemyPiece);
+        }
+      }
+      chessBoard.addPiece(startPosition, originalPiece);
+      return validMoves;
     }
   }
 
@@ -43,7 +72,7 @@ public class ChessGameImpl implements ChessGame {
       if (currColor != teamTurn) {
         throw new InvalidMoveException();
       }
-      Collection<ChessMove> possibleMoves = this.validMoves(move.getStartPosition());
+      Collection<ChessMove> possibleMoves = chessBoard.getPiece(move.getStartPosition()).pieceMoves(chessBoard, move.getStartPosition());
       boolean found = false;
 
       for (ChessMove moveCheck : possibleMoves) {
@@ -76,8 +105,6 @@ public class ChessGameImpl implements ChessGame {
 
   @Override
   public boolean isInCheck(TeamColor teamColor) {
-    Collection<ChessMove> movesThatCouldCheck= new HashSet<>();;
-    ChessPositionImpl kingPos=null;
     TeamColor opposingColor=null;
     if (teamColor == TeamColor.WHITE) {
       opposingColor = TeamColor.BLACK;
@@ -85,29 +112,46 @@ public class ChessGameImpl implements ChessGame {
       opposingColor = TeamColor.WHITE;
     }
 
-    for (int i = 1; i <= chessBoard.board.length; ++i) {
-      for (int j = 1; j <= chessBoard.board.length; ++j) {
-        ChessPositionImpl checkPos = new ChessPositionImpl(i, j);
-        if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getTeamColor() == opposingColor) {
-          movesThatCouldCheck.addAll(validMoves(checkPos));
-        } else if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getPieceType() == ChessPiece.PieceType.KING && chessBoard.getPiece(checkPos).getTeamColor() == teamColor) {
-          kingPos = checkPos;
+    Collection<ChessMove> movesThatCouldCheck= getCheckMoves(opposingColor);
+
+    if (getKingPos(teamColor) != null) { // this line is for testing purposes
+      for (ChessMove checkMove: movesThatCouldCheck) {
+        if (getKingPos(teamColor).equals(checkMove.getEndPosition())) {
+          return true;
         }
       }
-    }
-
-    for (ChessMove checkMove: movesThatCouldCheck) {
-      if (kingPos.equals(checkMove.getEndPosition())) {
-        return true;
-      }
+      return false;
     }
     return false;
   }
 
+  public Collection<ChessMove> getCheckMoves(TeamColor opposingColor) {
+    Collection<ChessMove> movesThatCouldCheck= new HashSet<>();
+    for (int i = 1; i <= chessBoard.board.length; ++i) {
+      for (int j = 1; j <= chessBoard.board.length; ++j) {
+        ChessPositionImpl checkPos = new ChessPositionImpl(i, j);
+        if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getTeamColor() == opposingColor) {
+          movesThatCouldCheck.addAll(chessBoard.getPiece(checkPos).pieceMoves(chessBoard, checkPos));
+        }
+      }
+    }
+    return movesThatCouldCheck;
+  }
+
+  public ChessPosition getKingPos(TeamColor teamColor) {
+    ChessPosition kingPos=null;
+    for (int i = 1; i <= chessBoard.board.length; ++i) {
+      for (int j = 1; j <= chessBoard.board.length; ++j) {
+        ChessPositionImpl checkPos = new ChessPositionImpl(i, j);if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getPieceType() == ChessPiece.PieceType.KING && chessBoard.getPiece(checkPos).getTeamColor() == teamColor) {
+          kingPos = checkPos;
+        }
+      }
+    }
+    return kingPos;
+  }
   @Override
   public boolean isInCheckmate(TeamColor teamColor) {
-    Collection<ChessMove> movesThatCouldCheckMate= new HashSet<>();;
-    ChessPositionImpl kingPos=null;
+    Collection<ChessMove> movesThatCouldCheckMate= new HashSet<>();
     TeamColor opposingColor=null;
     if (teamColor == TeamColor.WHITE) {
       opposingColor = TeamColor.BLACK;
@@ -115,25 +159,19 @@ public class ChessGameImpl implements ChessGame {
       opposingColor = TeamColor.WHITE;
     }
 
-    for (int i = 1; i <= chessBoard.board.length; ++i) {
-      for (int j = 1; j <= chessBoard.board.length; ++j) {
-        ChessPositionImpl checkPos = new ChessPositionImpl(i, j);
-        if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getPieceType() == ChessPiece.PieceType.KING && chessBoard.getPiece(checkPos).getTeamColor() == teamColor) {
-          kingPos = checkPos;
-        }
-      }
-    }
-    Collection<ChessMove> kingMoves = validMoves(kingPos);
+    ChessPosition kingPos = getKingPos(teamColor);
+    Collection<ChessMove> kingMoves = chessBoard.getPiece(kingPos).pieceMoves(chessBoard, kingPos);
 
     chessBoard.removePiece(kingPos);
     for (ChessMove kingMove: kingMoves) {
       ChessPieceImpl kingPiece=new ChessPieceImpl(teamColor, ChessPiece.PieceType.KING);
       chessBoard.addPiece(kingMove.getEndPosition(), kingPiece);
+      //FIXME
       for (int i=1; i <= chessBoard.board.length; ++i) {
         for (int j=1; j <= chessBoard.board.length; ++j) {
           ChessPositionImpl checkPos=new ChessPositionImpl(i, j);
           if (chessBoard.getPiece(checkPos) != null && chessBoard.getPiece(checkPos).getTeamColor() == opposingColor) {
-            movesThatCouldCheckMate.addAll(validMoves(checkPos));
+            movesThatCouldCheckMate.addAll(chessBoard.getPiece(checkPos).pieceMoves(chessBoard, checkPos));
           }
         }
       }
