094d68e57747e471ddcd43ef3f6f16bf293b2d61
chlohilt <hilton.chloe.y@gmail.com>
2023-10-09 15:39:28 -0600

    more phase 2 work

--- src/main/dataAccess/UserDAO.java
+++ src/main/dataAccess/UserDAO.java
@@ -1,4 +1,15 @@
 package dataAccess;
 
+import models.UserModel;
+
 public class UserDAO {
+  void createUser(UserModel u) throws DataAccessException {}
+
+  UserModel returnUser(String username) throws DataAccessException {
+    return null;
+  }
+
+  void updateUser(String username, UserModel u) throws DataAccessException {}
+
+  void deleteUser(UserModel u) throws DataAccessException {}
 }
