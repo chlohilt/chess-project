7e7c3e7025f26cc8bc70df279a0c2a96047f95cf
chlohilt <hilton.chloe.y@gmail.com>
2023-11-11 10:56:53 -0700

    repl

--- client/src/main/client/Repl.java
+++ client/src/main/client/Repl.java
@@ -1,10 +1,36 @@
 package client;
 
+import java.util.Scanner;
+import static ui.EscapeSequences.*;
+
 public class Repl {
+  private final ChessClient chessClient;
   public Repl(String serverUrl) {
-    
+    chessClient = new ChessClient(serverUrl);
   }
 
+  // TODO: add logger
   public void run() {
+    System.out.println("\uD83D\uDC36 Welcome to Chess Game Central. Sign in to start.");
+    System.out.print(chessClient.help());
+
+    Scanner scanner = new Scanner(System.in);
+    var result = "";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line = scanner.nextLine();
+
+      try {
+        result = chessClient.eval(line);
+        System.out.print(result);
+      } catch (Throwable e) {
+        System.out.print(e.getMessage());
+      }
+    }
+    System.out.println();
+  }
+
+  private void printPrompt() {
+    System.out.print("\n" + ">>> " + SET_BG_COLOR_GREEN);
   }
 }
